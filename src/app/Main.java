3af71e1e36bc8bf03754e8fde4b6b0ca413d78d9
Peniel <isnotjoke@gmail.com>
2014-06-07 10:11:31 -0500

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui/main.fxml"));
+        primaryStage.setTitle("Monitor");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("gui/img/mini.png"));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
